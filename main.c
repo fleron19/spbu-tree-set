a032c8f1b8fdc699c308c4c11b5d4247b7219b20
Mikhail Romanovskiy <romanovskymv@gmail.com>
2026-02-21 22:12:54 +0300

    Add main file with tests
    Co-authored-by: ermmmaks maks.ermoshkin56@gmail.com
    Co-authored-by: Guryanov-Kirill kg580652@gmail.com

---
+++ main.c
@@ -0,0 +1,106 @@
+#include "binary_searh_tree.h"
+#include <stdio.h>
+#include <string.h>
+
+#define GREEN(string) "\x1b[32m" string "\x1b[0m"
+#define RED(string) "\x1b[31m" string "\x1b[0m"
+
+bool testbstInsert_1()
+{
+    BST* bst = newBst();
+    bstInsert(bst, 5);
+    bstInsert(bst, 1);
+    bstInsert(bst, 6);
+    if (bst->root->value != 5) {
+        return false;
+    }
+    if (bst->root->leftChild->value != 1) {
+        return false;
+    }
+    if (bst->root->rightChild->value != 6) {
+        return false;
+    }
+
+    Node* rc = bst->root->rightChild;
+    Node* lc = bst->root->rightChild;
+    if (rc->leftChild || rc->rightChild || lc->leftChild || lc->rightChild) {
+        return false;
+    }
+
+    bstFree(bst);
+    return true;
+}
+
+bool testbstInsert_2()
+{
+    BST* bst = newBst();
+    bstInsert(bst, 1);
+    bstInsert(bst, 3);
+    bstInsert(bst, 3);
+    bstInsert(bst, 7);
+    if (bst->root->value != 1) {
+        return false;
+    }
+    if (bst->root->rightChild->value != 3) {
+        return false;
+    }
+    if (bst->root->rightChild->rightChild->value != 7) {
+        return false;
+    }
+    if (bst->root->leftChild) {
+        return false;
+    }
+    bstFree(bst);
+    return true;
+}
+
+bool testbstContains_1()
+{
+    BST* bst = newBst();
+    bstInsert(bst, 5);
+    bstInsert(bst, 1);
+    bstInsert(bst, 6);
+    bstInsert(bst, 8);
+    bool res = bstContains(bst, 8);
+    bstFree(bst);
+    return res;
+}
+
+bool testbstContains_2()
+{
+    BST* bst = newBst();
+    bstInsert(bst, 5);
+    bstInsert(bst, 1);
+    bstInsert(bst, 6);
+    bstInsert(bst, 8);
+    bool res = !bstContains(bst, 12);
+    bstFree(bst);
+    return res;
+}
+
+int main(int argc, char** argv)
+{
+    bool testMode = false;
+    for (int i = 0; i < argc; ++i) {
+        if (strcmp(argv[i], "--test") == 0) {
+            testMode = true;
+            break;
+        }
+    }
+
+    if (testMode) {
+        bool (*tests[4])() = { &testbstInsert_1, &testbstInsert_2,
+            &testbstContains_1, &testbstContains_2 };
+        for (int testNum = 0; testNum < 4; ++testNum) {
+            if (tests[testNum]()) {
+                printf(GREEN("Test %d passed!\n"), testNum + 1);
+            } else {
+                printf(RED("Test %d failed!\n"), testNum + 1);
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    return 0;
+}
